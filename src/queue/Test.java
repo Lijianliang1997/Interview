6b01ae1a8353e9ce41e7e4dc44b5ead692a497f2
lijianliang1997 <940104151@qq.com>
2018-05-28 21:15:59 +0800

    生产者消费者模型主函数

--- src/queue/Test.java
+++ src/queue/Test.java
@@ -9,11 +9,11 @@ import java.util.concurrent.LinkedBlockingDeque;
 public class Test {
     public static void main(String[] args) {
         BlockingQueue<String> queue = new LinkedBlockingDeque<>(6); // 不设置的话， LinkedBlockingQueue 默认大小为Integer.MAX_VALUE
-        Consumer consumer = new Consumer(queue);
-        Consumer consumer1 = new Consumer(queue);
-        Producer producer = new Producer(queue);
-        new Thread(producer).start();
-        new Thread(consumer).start();
-        new Thread(consumer1).start();
+        int n = 100;
+        for (int i = 0; i < n; i ++) {
+            new Thread(new Producer(queue), i+"").start();
+            new Thread(new Consumer(queue), i+"1").start();
+            new Thread(new Consumer(queue), i+"2").start();
+        }
     }
 }
