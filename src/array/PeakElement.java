6bd33440f8ed70ea579974b6c43e31d0e5fc1c7f
lijianliang1997 <940104151@qq.com>
2018-05-30 11:23:32 +0800

    修改后的版本

--- src/array/PeakElement.java
+++ src/array/PeakElement.java
@@ -7,17 +7,21 @@ public class PeakElement {
     public int findPeakElement(int[] nums) {
         int len = nums.length;
 
-        if( len ==1 || nums[0] > nums[1] )
+        if( len ==1 || nums[0] > nums[1] ) {
             return 0;
-        if( nums[len-2] < nums[len-1])
-            return len-1;
+        }
+        if( nums[len-2] < nums[len-1]) {
+            return len - 1;
+        }
         for( int i = 1 ; i < len-1 ; i++ ){
 
-            if( nums[i] > nums[i-1] && nums[i] > nums[i+1] )
+            if( nums[i] > nums[i-1] && nums[i] > nums[i+1] ) {
                 return i;
+            }
 
-            if( nums[i] > nums[i+1] )
+            if( nums[i] > nums[i+1] ) {
                 i++;
+            }
         }
         return 0;
     }
@@ -29,15 +33,18 @@ public class PeakElement {
             int mid = (l + r) >>> 1;
             // 如果mid-1大，那么peak肯定在l和mid-1之间（闭区间）  
             if (mid - 1 >= 0 &&
-                    nums[mid] < nums[mid - 1])
+                    nums[mid] < nums[mid - 1]) {
                 r = mid - 1;
+            }
                 // 如果mid+1大，那么peak肯定在mid+1和r之间（闭区间）  
             else if (mid + 1 < nums.length
-                    && nums[mid] < nums[mid + 1])
+                    && nums[mid] < nums[mid + 1]) {
                 l = mid + 1;
+            }
                 // 其他情况（peak在开头或者结尾，或者中间）  
-            else
+            else {
                 return mid;
+            }
         }
         return -1;
     }
