b23066c8bebe81bc90aded8c5d395eec3831dac2
lijianliang1997 <940104151@qq.com>
2018-05-31 19:27:47 +0800

    leetcode 200

--- src/graph/NumIslands.java
+++ src/graph/NumIslands.java
@@ -6,7 +6,10 @@ package graph;
 public class NumIslands {
     int m ,n;
     private int[][] direction = {{1, 0}, {-1, 0}, {0, 1}, {0 ,-1}};
-    public int numIsland (char[][] grid) {
+    public int numIslands (char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
         m = grid.length;
         n = grid[0].length;
         boolean[][] hasFind = new boolean[m][n];
@@ -23,7 +26,9 @@ public class NumIslands {
     }
 
     private void dfs (char[][] grid, int i, int j, boolean[][] hasFind) {
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0' || hasFind[i][j]) return;
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0' || hasFind[i][j]) {
+            return;
+        }
         if (grid[i][j] == '1') {
             hasFind[i][j] = true;
         }
@@ -40,6 +45,6 @@ public class NumIslands {
                         {'1' ,'1' ,'0', '1', '0'},
                          {'1' ,'1' ,'0','0' ,'0'},
                         {'0', '0' ,'0' ,'1' ,'1'}};
-        System.out.println(numIslands.numIsland(arrs));
+        System.out.println(numIslands.numIslands(arrs));
     }
 }
