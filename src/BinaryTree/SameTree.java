f1777240992faea9c41a5bde1200d07de94aad45
lijianliang1997 <940104151@qq.com>
2018-06-03 19:47:38 +0800

    leetcode 100

---
+++ src/BinaryTree/SameTree.java
@@ -0,0 +1,20 @@
+package BinaryTree;
+
+
+
+/**
+ * leetcode 100
+ * @author qq940
+ * @date 2018/6/3
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null && q != null || p != null && q == null || q.val != p.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
