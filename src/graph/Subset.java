5a412265644c6ee476bb852cac61fd5ab0897ec0
lijianliang1997 <940104151@qq.com>
2018-06-03 19:36:37 +0800

    leetcode 78

--- src/graph/Subset.java
+++ src/graph/Subset.java
@@ -4,36 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by qq940 on 2018/3/31.
+ * leetcode 78
+ * @author qq940
+ * @date 2018/3/31
  */
 public class Subset {
-//    public List<List<Integer>> subsets (int[] nums) {
-//        List<List<Integer>> ret = new ArrayList<>();
-//        List<Integer> subsetList = new ArrayList<>();
-//        int size = nums.length;
-//        for (int i = 0; i < size; i ++) {
-//            backtracking(nums, i,0, subsetList, ret );
-//        }
-//        return ret;
-//    }
-//
-//    public void backtracking (int[] nums, int size,int start, List<Integer> subsetList, List<List<Integer>> ret) {
-//        if (subsetList.size() == size) {
-//            ret.add(new ArrayList<>(subsetList));
-//            return;
-//        }
-//        for (int i = start; i < nums.length; i ++) {
-//            subsetList.add(nums[i]);
-//            backtracking(nums, size, i + 1, subsetList, ret);
-//            subsetList.remove(subsetList.size() - 1);
-//        }
-//    }
-
-    public List<List<Integer>> subsets (int [] nums) {
+    public List<List<Integer>> subsets (int[] nums) {
         List<List<Integer>> ret = new ArrayList<>();
         List<Integer> subsetList = new ArrayList<>();
         int size = nums.length;
-        for (int i = 0; i < size; i ++) {
+        for (int i = 0; i <= size; i ++) {
             backtracking(nums,i, 0, subsetList, ret);
         }
         return ret;
@@ -46,8 +26,17 @@ public class Subset {
         }
         for (int i = start; i < nums.length; i ++) {
             subsetList.add(nums[i]);
-            backtracking(nums, size, i + 1, subsetList,ret);
+            backtracking(nums, size, i + 1, subsetList, ret);
             subsetList.remove(subsetList.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Subset subset = new Subset();
+        int[] arr = {1, 2, 3};
+        List<List<Integer>> subsetList = subset.subsets(arr);
+        for (List<Integer> sub : subsetList) {
+            System.out.println(sub.toString());
         }
     }
 }
