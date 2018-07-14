008d93359e054aaa05133b62ee674f52f2e37a71
lijianliang1997 <940104151@qq.com>
2018-07-14 13:40:50 +0800

    复杂链表

---
+++ src/ListNode/RandomListNode.java
@@ -0,0 +1,15 @@
+package ListNode;
+
+/**
+ * @author lijianliang
+ * @date 2018/7/14.
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
