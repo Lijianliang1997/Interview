ba8314e6449be7ad5bfd22878fb2472f06e25d92
lijianliang1997 <940104151@qq.com>
2018-07-31 16:13:26 +0800

    kmp算法

--- src/string/KMP.java
+++ src/string/KMP.java
@@ -1,47 +1,60 @@
 package string;
 
 /**
- * Created by qq940 on 2018/4/7.
+ * @author lijianliang
+ * @date 2018/7/31.
  */
 public class KMP {
-    public  int kmp (String str, String sub) {
+
+    public int kmp (String str, String sub) {
         if(str == null || sub == null || str.length() == 0 || sub.length() == 0){
             throw new IllegalArgumentException("str或者sub不能为空");
         }
-        int i =0, j =0;
+        int i = 0;
+        int j = 0;
         int[] next = next(sub);
         while (j < next.length && i < str.length()) {
-            if (j == -1 || str.charAt(i) == sub.charAt(i)) {
+            if (str.charAt(i) == sub.charAt(j)) {
                 i ++;
                 j ++;
-            } else {
+            } else if (next[j] == -1) {
+                i ++;
+            }
+            else {
                 j = next[j];
             }
         }
-        if ( j < next.length){
+        if (j < next.length) {
             return -1;
         } else {
             return i - sub.length();
         }
     }
-
     public int[] next (String sub) {
-        int [] next = new int[sub.length()];
-        int len = 0;
-        int i ,j;
-        i = 0;
-        j = next[0] = 1;
-        while (i < len - 1) {
-            while (j != -1 && sub.charAt(i) != sub.charAt(j)) {
+        if (sub.length() == 1) {
+            return new int[] {-1};
+        }
+        int len = sub.length();
+        int[] next = new int[len];
+        next[0] = -1;
+        next[1] = 0;
+        int i =2, j = 0;
+        while (i < len ) {
+            if (sub.charAt(i - 1) == sub.charAt(j)) {
+                next[i ++] = ++ j;
+            } else if (j > 0) {
                 j = next[j];
+            } else {
+                next[i ++] = 0;
             }
-            next[++i] = ++j;
         }
         return next;
     }
 
     public static void main(String[] args) {
         KMP kmp = new KMP();
-        System.out.println(kmp.kmp("hello", "ll"));
+        String str = "mississippi";
+        String sub = "issip";
+        System.out.println(kmp.kmp(str,sub));
     }
 }
